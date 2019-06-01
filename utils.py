b1d3702dc5c2bdc42fe6422a31a788b4129900ec
Roberto Franceschini <franceschini.roberto@gmail.com>
2019-06-02 00:29:15 +0200

    histo2D

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 # coding: utf-8 
 # # Import
 
-# In[137]:
+# In[210]:
 
 
 from __future__ import print_function
@@ -177,11 +177,18 @@ def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
     return texts
 
 
+# In[189]:
+
+
+def histo2D(data, row_labels, col_labels, ax=None, cbarlabel="",offsetx=0,offsety=-0.5, reverse=True,transpose=True, **kwargs):
+    return heatmap(data, row_labels, col_labels, ax=ax, cbarlabel=cbarlabel,offsetx=offsetx,offsety=offsety, reverse=reverse,transpose=transpose, **kwargs) 
+
+
 # In[130]:
 
 
 def heatmap(data, row_labels, col_labels, ax=None,
-            cbar_kw={},grid_kw={}, cbarlabel="",offsetx=0,offsety=0, **kwargs):
+            cbar_kw={},grid_kw={}, cbarlabel="",offsetx=0,offsety=0, reverse=False,transpose=False, **kwargs):
     """
     Create a heatmap from a numpy array and two lists of labels.
 
@@ -200,12 +207,18 @@ def heatmap(data, row_labels, col_labels, ax=None,
         cbarlabel  : The label for the colorbar
     All other arguments are directly passed on to the imshow call.
     """
-
+    
+   
+    rev=2*int(False)-1 # 1 if True -1 if False
+    if transpose:
+        data=np.transpose(data)
     if not ax:
         ax = plt.gca()
 
+       
     # Plot the heatmap
-    im = ax.imshow(data, **kwargs)
+    im = ax.imshow(data[::rev], **kwargs)
+    
 
     # Create colorbar
     cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
@@ -216,7 +229,7 @@ def heatmap(data, row_labels, col_labels, ax=None,
     ax.set_yticks(np.arange(data.shape[0])-offsety)
     # ... and label them with the respective list entries.
     ax.set_xticklabels(col_labels)
-    ax.set_yticklabels(row_labels)
+    ax.set_yticklabels(row_labels[::rev])
 
     # Let the horizontal axes labeling appear on top.
     ax.tick_params(top=True, bottom=False,
@@ -442,7 +455,7 @@ def bt(a,b):
 
 # # File I/O
 
-# In[138]:
+# In[211]:
 
 
 def read_file_to_lines(file_name):
@@ -453,7 +466,7 @@ def read_file_to_lines(file_name):
     return _xml_groups
 
 
-# In[139]:
+# In[212]:
 
 
 def write_lines_to_file(mylines,filename,mode='a',final_line=False):
@@ -464,7 +477,7 @@ def write_lines_to_file(mylines,filename,mode='a',final_line=False):
         thefile.write("\n")      
 
 
-# In[140]:
+# In[213]:
 
 
 def write_lines_to_file_newline(mylines,filename,mode='a'):
@@ -473,7 +486,7 @@ def write_lines_to_file_newline(mylines,filename,mode='a'):
           thefile.write("\n%s" % item)
 
 
-# In[141]:
+# In[214]:
 
 
 def filejson2dictionary(fn):
@@ -482,7 +495,7 @@ def filejson2dictionary(fn):
     return d
 
 
-# In[142]:
+# In[215]:
 
 
 def change_tag_in_file(filename=None,tag=None,text=None):
@@ -549,7 +562,7 @@ def measurementFromString(s,err='±'):
     return list(map(lambda x: float(x), s.split(err) ) )
 
 
-# In[143]:
+# In[216]:
 
 
 def get_best_match(query, corpus, step=4, flex=3, case_sensitive=False, verbose=False):
@@ -656,21 +669,21 @@ def get_best_match(query, corpus, step=4, flex=3, case_sensitive=False, verbose=
 
 # # Lists
 
-# In[144]:
+# In[217]:
 
 
 def sort_by_ith(data,i):
     return sorted(data, key=lambda tup: tup[i])
 
 
-# In[145]:
+# In[218]:
 
 
 def flattenOnce(tags_times):
     return [y for x in tags_times for y in x]
 
 
-# In[146]:
+# In[219]:
 
 
 def arange(a,b,s):
@@ -687,21 +700,21 @@ linspace(0,2,0.2)
 
 # # Strings
 
-# In[147]:
+# In[220]:
 
 
 def remove_multiple_spaces(string):
     return re.sub(' +',' ',string)
 
 
-# In[148]:
+# In[221]:
 
 
 def ToString(x):
     return str(x)
 
 
-# In[149]:
+# In[222]:
 
 
 def dashed_to_year(stri):
@@ -739,7 +752,7 @@ def dashed_to_year(stri):
 
 # # Dictionaries
 
-# In[150]:
+# In[223]:
 
 
 def dict2string(dictio):
@@ -758,7 +771,7 @@ def logticks(basis=[1,2,5],orders=[-1.,-2.,-3.,-4.]):
     return np.array(list(map(lambda x: np.array(basis)*np.power(10,x),np.array(orders) ))).flatten()
 
 
-# In[151]:
+# In[224]:
 
 
 def num(s):
@@ -796,7 +809,7 @@ def sci_notation(num, decimal_digits=1, precision=None, exponent=None):
     return r"${0:.{2}f}\cdot10^{{{1:d}}}$".format(coeff, exponent, precision)
 
 
-# In[152]:
+# In[225]:
 
 
 def to_precision(x,p):
